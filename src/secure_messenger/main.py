984d7eec3a8b062d1e56366263ac73d6809e32b0
jarzeckil <jarzeckil@gmail.com>
2026-01-10 19:36:59 +0100

    feature: added routers and lifespan func

--- src/secure_messenger/main.py
+++ src/secure_messenger/main.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI
-from src.secure_messenger.core.config import settings
-import uvicorn
+from contextlib import asynccontextmanager
 
-app = FastAPI(title=settings.PROJECT_NAME)
+from fastapi import FastAPI
+from src.secure_messenger.auth.router import auth_router
+from src.secure_messenger.core.config import settings
+from src.secure_messenger.db.init_db import init_db
+
+from secure_messenger.db.database import engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # initialize tables if they don't exist
+    await init_db()
+
+    yield
+
+    # app closes
+    engine.dispose()
+
+
+app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
+app.include_router(auth_router)
 
 
 @app.get('/health')
 async def health_check():
     return {'status': 'ok', 'service': 'Secure Messenger'}
-
-
-if __name__ == '__main__':
-    uvicorn.run('src.main:app', host='0.0.0.0', port=8000, reload=True)
