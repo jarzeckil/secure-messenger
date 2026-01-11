395a2e9408ceda05650b50aa76cb923296ae09b4
jarzeckil <jarzeckil@gmail.com>
2026-01-11 18:30:21 +0100

    feature: logging endpoint returns session id as a cookie

--- src/secure_messenger/auth/router.py
+++ src/secure_messenger/auth/router.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.responses import Response
+import redis.asyncio as redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from secure_messenger.auth.schemas import UserLoginModel, UserRegisterModel
 from secure_messenger.auth.service import login_user, register_user
+from secure_messenger.core.config import settings
 from secure_messenger.db.database import get_db
+from secure_messenger.db.redis_client import get_redis
 
 auth_router = APIRouter()
 
@@ -20,9 +24,21 @@ async def register(user_data: UserRegisterModel, db: AsyncSession = Depends(get_
 
 
 @auth_router.post('/login', status_code=status.HTTP_200_OK)
-async def login(user_login_data: UserLoginModel, db: AsyncSession = Depends(get_db)):
-    await login_user(db, user_login_data)
+async def login(
+    response: Response,
+    user_login_data: UserLoginModel,
+    db: AsyncSession = Depends(get_db),
+    redis_client: redis.Redis = Depends(get_redis),
+):
+    session_id = await login_user(db, redis_client, user_login_data)
 
-    # TODO cookies
+    response.set_cookie(
+        key='session_id',
+        value=f'{session_id}',
+        httponly=True,
+        secure=True,
+        samesite='lax',
+        max_age=settings.MAX_SESSION_AGE,
+    )
 
     return {'message': 'Logged-in successfully'}
