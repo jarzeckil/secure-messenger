2c07790d231b60513fc7248a2a7018b404bbe075
jarzeckil <jarzeckil@gmail.com>
2026-01-13 18:50:16 +0100

    feature: created get messages endpoint

--- src/secure_messenger/messages/router.py
+++ src/secure_messenger/messages/router.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter, Depends, Request, status
+from fastapi import APIRouter, Depends, Query, Request, status
 import redis.asyncio as redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from secure_messenger.auth.dependencies import get_current_user
 from secure_messenger.db.database import get_db
 from secure_messenger.db.redis_client import get_redis
-from secure_messenger.messages.schemas import SendMessageModel
-from secure_messenger.messages.service import send_message
+from secure_messenger.messages.schemas import SendMessageModel, ShowMessageModel
+from secure_messenger.messages.service import get_user_messages, send_message
 
 messages_router = APIRouter()
 
@@ -23,3 +23,22 @@ async def send(
     missing_users = await send_message(db, current_user, message_data)
 
     return {'message': 'Message sent successfully', 'missing users': missing_users}
+
+
+@messages_router.get(
+    '/messages/get',
+    response_model=list[ShowMessageModel],
+    status_code=status.HTTP_200_OK,
+)
+async def get_inbox(
+    request: Request,
+    db: AsyncSession = Depends(get_db),
+    redis_client: redis.Redis = Depends(get_redis),
+    skip: int = Query(0, ge=0, description='Number of messages to skip'),
+    limit: int = Query(50, ge=1, le=100, description='Number of messages to download'),
+):
+    current_user = await get_current_user(request, redis_client)
+
+    messages = await get_user_messages(db, current_user, skip, limit)
+
+    return messages
