de75b27d815b332d03d678d5005503b107fa7e40
jarzeckil <jarzeckil@gmail.com>
2026-01-13 18:50:21 +0100

    feature: created get messages endpoint

--- src/secure_messenger/messages/schemas.py
+++ src/secure_messenger/messages/schemas.py
@@ -1,6 +1,17 @@
+from datetime import datetime
+from uuid import UUID
+
 from pydantic import BaseModel, Field
 
 
 class SendMessageModel(BaseModel):
     recipients: list[str] = Field(description='List of recipient usernames')
     text_message: str = Field(description='Text message', min_length=1)
+
+
+class ShowMessageModel(BaseModel):
+    id: UUID
+    sender_username: str
+    text_content: str
+    is_read: bool
+    timestamp: datetime
