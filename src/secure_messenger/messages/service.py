cfa2fda30cea308b448ed9b113949709378fb417
jarzeckil <jarzeckil@gmail.com>
2026-01-14 21:11:56 +0100

    feature: added attachments to send message and verify message

--- src/secure_messenger/messages/service.py
+++ src/secure_messenger/messages/service.py
@@ -1,14 +1,16 @@
 from collections.abc import Sequence
 import logging
+import uuid
 
-from fastapi import HTTPException, status
+from fastapi import HTTPException, UploadFile, status
 from sqlalchemy import delete, desc, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from secure_messenger.auth.dependencies import CurrentUser
 from secure_messenger.core import security
-from secure_messenger.db.models import Message, MessageRecipient, User
+from secure_messenger.core.config import settings
+from secure_messenger.db.models import Attachment, Message, MessageRecipient, User
 from secure_messenger.messages.schemas import (
     MessageIDModel,
     SendMessageModel,
@@ -19,45 +21,91 @@ logger = logging.getLogger(__name__)
 
 
 async def send_message(
-    db: AsyncSession, current_user: CurrentUser, message_data: SendMessageModel
+    db: AsyncSession,
+    current_user: CurrentUser,
+    message_data: SendMessageModel,
+    files: list[UploadFile],
 ) -> list[str]:
+    if len(files) > 5:
+        raise HTTPException(status_code=400, detail='Max file count: 5')
+
     logger.info(f'Sending message from {current_user.user_id}')
 
-    recipient_usernames = set(message_data.recipients)
-    query = select(User).where(User.username.in_(recipient_usernames))
-    result = await db.execute(query)
-    found_recipients: Sequence[User] = result.scalars().all()
-    found_recipient_usernames = {recipient.username for recipient in found_recipients}
+    try:
+        # check for missing users
+        recipient_usernames = set(message_data.recipients)
+        query = select(User).where(User.username.in_(recipient_usernames))
+        result = await db.execute(query)
+        found_recipients: Sequence[User] = result.scalars().all()
+        found_recipient_usernames = {
+            recipient.username for recipient in found_recipients
+        }
 
-    missing_users = list(recipient_usernames - found_recipient_usernames)
+        missing_users = list(recipient_usernames - found_recipient_usernames)
 
-    if missing_users:
-        logger.warning(f'Users do not exist: {missing_users}')
-    if not found_recipients:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='Users do not exist'
+        if missing_users:
+            logger.warning(f'Users do not exist: {missing_users}')
+        if not found_recipients:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail='Users do not exist'
+            )
+
+        # generate message constants
+        message_id = uuid.uuid4()
+        aes_key = security.generate_random_aes_key()
+        signature_content = []
+
+        # create attachments
+        for file in files:
+            if file.size > settings.MAX_FILE_SIZE:
+                raise HTTPException(
+                    status_code=status.HTTP_413_CONTENT_TOO_LARGE,
+                    detail='Max file size: 10 MB',
+                )
+            content = await file.read()
+            content_encrypted = security.encrypt_content(content, aes_key)
+            content_hash = security.get_content_hash(content_encrypted)
+            new_attachment = Attachment(
+                message_id=message_id,
+                data_encrypted=content_encrypted,
+                encrypted_data_hash=content_hash,
+                filename=file.filename,
+                content_type=file.content_type,
+                size=len(content),
+            )
+            db.add(new_attachment)
+
+            # add attachment content to signature data
+            signature_content.append(content_hash)
+
+        # encrypt text message and add it to signature data
+        text_message = message_data.text_message
+        encrypted_text_message = security.encrypt_content(
+            text_message.encode(), aes_key
+        )
+        signature_content.append(encrypted_text_message)
+
+        # create signature
+        signature_blob = b''.join(sorted(signature_content))
+        signature = security.generate_signature(
+            signature_blob, current_user.private_key
         )
 
-    text_message = message_data.text_message
-    aes_key = security.generate_random_aes_key()
-    encrypted_text_message = security.encrypt_content(text_message.encode(), aes_key)
-    signature = security.generate_signature(
-        encrypted_text_message, current_user.private_key
-    )
-    try:
+        # create message object
         new_message = Message(
+            id=message_id,
             sender_id=current_user.user_id,
             content_encrypted=encrypted_text_message,
             signature=signature,
         )
         db.add(new_message)
-        await db.flush()
 
+        # create recipients
         for recipient in found_recipients:
             encrypted_aes_key = security.encrypt_aes_key(aes_key, recipient.public_key)
 
             new_message_recipient = MessageRecipient(
-                message_id=new_message.id,
+                message_id=message_id,
                 recipient_id=recipient.id,
                 encrypted_message_key=encrypted_aes_key,
             )
@@ -151,7 +199,10 @@ async def verify_message(
 ):
     query = (
         select(MessageRecipient)
-        .options(joinedload(MessageRecipient.message).joinedload(Message.sender))
+        .options(
+            joinedload(MessageRecipient.message).joinedload(Message.sender),
+            joinedload(MessageRecipient.message).joinedload(Message.attachments),
+        )
         .where(
             MessageRecipient.recipient_id == current_user.user_id,
             MessageRecipient.message_id == verif_message.message_id,
@@ -165,9 +216,15 @@ async def verify_message(
             status_code=status.HTTP_404_NOT_FOUND, detail='Message not found'
         )
 
+    signature_content = [recipient.message.content_encrypted]
+
+    for attachment in recipient.message.attachments:
+        signature_content.append(attachment.encrypted_data_hash)
+    signature_blob = b''.join(sorted(signature_content))
+
     try:
         security.verify_signature(
-            recipient.message.content_encrypted,
+            signature_blob,
             recipient.message.sender.public_key,
             recipient.message.signature,
         )
